a59a5a1d2c6e9955e481bc5aa6367639f0c1e2f2
JhowDiones <46484335+JhowDiones@users.noreply.github.com>
2021-08-30 17:05:13 -0300

    att

--- UpdateManute/Form1.Designer.cs
+++ UpdateManute/Form1.Designer.cs
@@ -29,20 +29,47 @@ namespace UpdateManute
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txt_informacao = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 79);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(454, 36);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // txt_informacao
+            // 
+            this.txt_informacao.AutoSize = true;
+            this.txt_informacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_informacao.Location = new System.Drawing.Point(9, 9);
+            this.txt_informacao.Name = "txt_informacao";
+            this.txt_informacao.Size = new System.Drawing.Size(459, 24);
+            this.txt_informacao.TabIndex = 1;
+            this.txt_informacao.Text = "Agurade enquanto o gerenciador baixa a atualização!";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(380, 197);
+            this.ClientSize = new System.Drawing.Size(478, 157);
+            this.Controls.Add(this.txt_informacao);
+            this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update - GTManute";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label txt_informacao;
     }
 }
 
--- UpdateManute/Form1.cs
+++ UpdateManute/Form1.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using Ionic.Zip;
+using System;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
 
 namespace UpdateManute
@@ -14,6 +11,74 @@ namespace UpdateManute
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BtnDownload_Click();
+        }
+        
+        private void BtnDownload_Click()
+        {
+
+            string path = @"c:\GTManute\Updates\InstallManute.zip";
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                    wc.DownloadFileAsync(
+                        // Param1 = Link of file
+                        new System.Uri("https://j-tec.000webhostapp.com/GTManuteUp/InstallManute.zip"),
+                        // Param2 = Path to save
+                        "C:\\GTManute\\Updates\\InstallManute.zip"
+
+                    );
+                    Descompactar();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        // Event to track the progress
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage - 10;
+        }
+        public void Descompactar()
+        {
+            ZipFile arquivoZip = ZipFile.Read("C:\\GTManute\\Updates\\InstallManute.zip");
+            try
+            {
+                foreach (ZipEntry e in arquivoZip)
+                {
+                    e.Extract("C:\\GTManute\\Updates");
+                    progressBar1.Value = 100;
+                    MessageBox.Show("Instalador extraido com sucesso! Finalize a atualização pelo instalador!!!");
+                }
+                arquivoZip.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
